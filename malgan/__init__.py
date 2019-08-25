d56cb45359cd4063404277e0398a0af3587ac930
Bedang Sen <bedang247@gmail.com>
2019-08-25 07:20:21 +0400

    Set directories for ADversarialy generated vectors

--- malgan/__init__.py
+++ malgan/__init__.py
@@ -373,6 +373,12 @@ class MalGAN(nn.Module):
 
         :return: Results information as a comma separated string
         """
+
+        adversarial_feature_vector_directory = "adversarial_feature_vector_directory"
+
+        if not os._exists(adversarial_feature_vector_directory):
+            os.mkdir(adversarial_feature_vector_directory)
+
         # noinspection PyTypeChecker
         valid_loss = self._meas_loader_gen_loss(self._mal_data.valid)
         # noinspection PyTypeChecker
@@ -393,8 +399,6 @@ class MalGAN(nn.Module):
 
             m_diff = m_prime - m
             bits_changed.append(torch.sum(m_diff.cpu(), dim=1))
-
-            pickle.dump(m_diff, open("adversarial_feature_set.pk", 'wb'))
 
             # logging.debug("The shape of m_diff is : " + str(m_diff.shape))
             # logging.debug("The type of m_diff is : " + str(type(m_diff)))
